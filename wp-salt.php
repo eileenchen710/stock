522374c13e67c37be7da72a96cc23d2766c650b5
Vygox <vygoxau@gmail.com>
2026-01-14 05:10:26 +0000

    Initial WordPress setup for stock management system

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'XcUbQfB0Ma14onnXbLPpNWgJpUIft55ezbrS0BiFoTYc51HMpQVRW5GmeytPoQ94');
+define('SECURE_AUTH_KEY',  'aJ5AfDUPWWNGhh8wJqgzPh5LJRv2jnz7M5q2XodG07Pf5tijSgEKu75uSuEW3dNW');
+define('LOGGED_IN_KEY',    'hvgwr7dxuaSu8rKho5YtSutwJNhxn429J7LPjs23vWFsGzimtQWqnaeSSBqsiozs');
+define('NONCE_KEY',        'uyp4cJVUXRtGuKURwUhycBdYpeuJ9LFqaCEYgWAdWAaWLiFwe8egXY2LsXmT8U0Q');
+define('AUTH_SALT',        '0UFcbXzuErE678PncHRRVYXrruN1qqvHcKMaFTeoMmivQiEHrUzB0mTNjo52qvdr');
+define('SECURE_AUTH_SALT', 'shQjP07owIIUriYbtKWszcX3BcvwttLwercj8HIpijjUsvi7PL5T1znQoy4NbhvB');
+define('LOGGED_IN_SALT',   'LXUSmyS63zJ5dPydJ5Qt3IVvrTngLbfm9m3jWP6zKjcatctL6uySbjEURMINt3tG');
+define('NONCE_SALT',       '5nTWX93wIX1Y07zntLjPqhh1gs3aVfiEEXKyncHhunxwqev1Hpe1IFo6YX5uCJp8');
